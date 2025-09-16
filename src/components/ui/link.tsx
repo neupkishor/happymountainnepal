b97728ab2bbbb75b1c3fe2633b5294d5eee738b6
Kishor Neupane <heyneupkishor@gmail.com>
2025-09-16 11:41:44 +0000

    on likes like # or on tabs that open in new tab, or where the page does

--- src/components/ui/link.tsx
+++ src/components/ui/link.tsx
@@ -4,6 +4,8 @@
 import NextLink from 'next/link';
 import { cn } from '@/lib/utils';
 import type { ComponentProps } from 'react';
+import NProgress from 'nprogress';
+import { usePathname } from 'next/navigation';
 
 type LinkProps = ComponentProps<typeof NextLink> & {
     children: React.ReactNode;
@@ -11,9 +13,27 @@ type LinkProps = ComponentProps<typeof NextLink> & {
 }
 
 export const Link = ({ href, className, children, ...props }: LinkProps) => {
+  const pathname = usePathname();
+
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    const currentPath = pathname;
+    const newPath = href.toString();
+    
+    // Check for new tab clicks
+    if (props.target === '_blank' || e.ctrlKey || e.metaKey) {
+      return;
+    }
+
+    // Check for anchor links or links to the same page
+    if (newPath.startsWith('#') || newPath === currentPath) {
+      return;
+    }
+
+    NProgress.start();
+  };
 
   return (
-    <NextLink href={href} className={cn('hover:text-primary', className)} {...props}>
+    <NextLink href={href} className={cn('hover:text-primary', className)} onClick={handleClick} {...props}>
       {children}
     </NextLink>
   );
