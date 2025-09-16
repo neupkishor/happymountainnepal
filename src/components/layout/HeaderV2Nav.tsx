ba3282a15f8ff9ac58e62efea586388d66f63599
Kishor Neupane <heyneupkishor@gmail.com>
2025-09-16 07:14:02 +0000

    why is the component shrinking from the time i hover and get the dropdow

--- src/components/layout/HeaderV2Nav.tsx
+++ src/components/layout/HeaderV2Nav.tsx
@@ -29,25 +29,49 @@ const hasChildren = (item: NavLink): item is Required<Pick<NavLink, 'children'>>
 export const HeaderV2Nav = ({ links }: HeaderV2NavProps) => {
   const pathname = usePathname();
 
-  const renderLinks = (links: NavLink[]): React.ReactNode => {
+  const renderNavLinks = (links: NavLink[]): React.ReactNode => {
     return links.map((link) => {
       if (hasChildren(link)) {
         return (
           <NavigationMenuPrimitive.Item key={link.title}>
-            <NavigationMenuPrimitive.Trigger className={navigationMenuTriggerStyle()}>
+            <NavigationMenuPrimitive.Trigger className={cn(navigationMenuTriggerStyle(), "group")}>
               {link.title}
+              <ChevronDown
+                className="relative top-[1px] ml-1 h-3 w-3 transition duration-200 group-data-[state=open]:rotate-180"
+                aria-hidden="true"
+              />
             </NavigationMenuPrimitive.Trigger>
             <NavigationMenuPrimitive.Content>
-              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                {link.children!.map((component) => (
-                    <ListItem
-                        key={component.title}
-                        title={component.title}
-                        href={component.href}
-                    >
-                        {component.description}
-                    </ListItem>
-                ))}
+                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                    {link.children.map((component) => {
+                        if (hasChildren(component)) {
+                            return (
+                                <li key={component.title} className="row-span-3">
+                                    <NavigationMenuPrimitive.Link asChild>
+                                    <div
+                                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                                    >
+                                        <div className="mb-2 mt-4 text-lg font-medium">
+                                        {component.title}
+                                        </div>
+                                        <p className="text-sm leading-tight text-muted-foreground">
+                                        {component.description}
+                                        </p>
+                                    </div>
+                                    </NavigationMenuPrimitive.Link>
+                                </li>
+                            )
+                        }
+                        return (
+                            <ListItem
+                                key={component.title}
+                                title={component.title}
+                                href={component.href}
+                            >
+                                {component.description}
+                            </ListItem>
+                        )
+                    })}
               </ul>
             </NavigationMenuPrimitive.Content>
           </NavigationMenuPrimitive.Item>
@@ -77,10 +101,10 @@ export const HeaderV2Nav = ({ links }: HeaderV2NavProps) => {
       className="relative z-10 flex max-w-max flex-1 items-center justify-center"
     >
       <NavigationMenuPrimitive.List className="group flex flex-1 list-none items-center justify-center space-x-1">
-        {renderLinks(links)}
+        {renderNavLinks(links)}
       </NavigationMenuPrimitive.List>
 
-      <div className="absolute top-full flex justify-center">
+      <div className="perspective-[2000px] absolute top-full flex justify-center">
         <NavigationMenuPrimitive.Viewport
           className={cn(
             "origin-top-center relative mt-1.5 h-[var(--radix-navigation-menu-viewport-height)] w-full overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 md:w-[var(--radix-navigation-menu-viewport-width)]"
@@ -140,3 +164,4 @@ export function MobileNavLink({ href, children, pathname, setOpen }: MobileLinkP
     </Link>
   );
 }
+
