c52072202a5733125b81b5c7cd0ae3f4fb2add91
Kishor Neupane <neupkishor.3@gmail.com>
2025-10-20 01:59:18 +0000

    apiKey: "AIzaSyCeD4j0I-O9lL_LFWxFPDacguz_BAFdxmo",   authDomain: "studio

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-2509040848-3ec7e",
-  "appId": "1:567153279956:web:5df81b492d0fc37ac1cc41",
-  "apiKey": "AIzaSyCeD4j0I-O9lL_LFWxFPDacguz_BAFdxmo",
-  "authDomain": "studio-2509040848-3ec7e.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "567153279956"
+  apiKey: "AIzaSyCeD4j0I-O9lL_LFWxFPDacguz_BAFdxmo",
+  authDomain: "studio-2509040848-3ec7e.firebaseapp.com",
+  projectId: "studio-2509040848-3ec7e",
+  storageBucket: "studio-2509040848-3ec7e.appspot.com",
+  messagingSenderId: "567153279956",
+  appId: "1:567153279956:web:5df81b492d0fc37ac1cc41"
 };
